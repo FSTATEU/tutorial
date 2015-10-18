225d9116cefb0b4d4b0aa1497a92854441cae738
Chekad Sarami <csarami@gmail.com>
2015-10-18 07:46:33 -0400

    created skeleton.py

---
+++ skeleton.py
@@ -0,0 +1,21 @@
+"""
+Module Docment
+"""
+
+#
+## Implement increment function inc(x) here
+#
+
+#
+## Implement  function double(x) which returns 2*x, here
+#
+
+
+
+def main():
+    """Testing functions"""
+    x = 1;
+    print( inc(x)*double(x))
+
+if __name__=='__main__':
+    test()
